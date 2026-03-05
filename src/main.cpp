9716ad8006603c1cd6f6ec6b132cbfd0d1034173
Incidius <89553616+Incidius@users.noreply.github.com>
2026-03-05 12:17:02 +0000

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,9 @@
-/**
- * Include the Geode headers.
- */
+
 #include <Geode/Geode.hpp>
 
 #include <Geode/modify/CCEGLView.hpp>
 
-// Win32 headers for focus detection
+
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
@@ -16,34 +14,34 @@ class $modify(CCEGLView) {
 		CCEGLView::pollEvents();
 
 		static bool wasFocused = true;
-		// Storage for volumes when we alt-tab out
+		
 		static float savedMusicVol = 1.0f;
 		static float savedSfxVol = 1.1f;
 
 		HWND gdWindow = WindowFromDC(wglGetCurrentDC());
 		bool isFocused = (GetActiveWindow() == gdWindow);
 
-		// Get the engine instance using sharedEngine() from your header
+		
 		auto engine = FMODAudioEngine::sharedEngine();
 
 		if (!isFocused && wasFocused) {
-			// --- LOSS OF FOCUS ---
-			// 1. Save current volume levels from the member variables
+			
 			savedMusicVol = engine->m_musicVolume;
 			savedSfxVol = engine->m_sfxVolume;
 
-			// 2. Mute using the setter functions found in your header
+			
 			engine->setBackgroundMusicVolume(0.0f);
 			engine->setEffectsVolume(0.0f);
+			//mutes game when it detects it is unfocused
 
 			
 		}
 		else if (isFocused && !wasFocused) {
-			// --- GAIN OF FOCUS ---
-			// Restore the volumes we saved earlier
-			if (savedSfxVol <= 1.0f) {
+			
+			if (savedSfxVol <= 1.0f) { //fixes a bug where the game doesn't have a volume set when opening and sets it to max volume
 				engine->setBackgroundMusicVolume(savedMusicVol);
 				engine->setEffectsVolume(savedSfxVol);
+				//when gd is refocused, it turns the volume back to what it was before
 			}
 
 			
